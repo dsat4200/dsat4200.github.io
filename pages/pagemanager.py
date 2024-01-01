ece7c7b19e2912867a1a2b4aae36abbe731a52cf
dsat4200 <kudosmydude@gmail.com>
2024-01-01 14:09:34 -0700

    push
    the first one

---
+++ pages/pagemanager.py
@@ -0,0 +1,73 @@
+# Number of pages you want to generate
+num_pages = 13
+
+# Loop to generate pages
+for page in range(2, num_pages+1):
+    # Define the previous and next page numbers
+    prev_page = page - 1
+    next_page = page + 1
+
+    # HTML template for each page
+    html_template = f'''
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Read Joules + Igor</title>
+    <link rel="stylesheet" href="../style.css">
+</head>
+
+<body>
+    <header>
+        <h1 class="header_img">
+            Joules + Igor
+        </h1>
+        <br>
+        <nav>
+            <ul>
+                <li><a href="../index.html">Home</a></li>
+                <li><a href="../about.html">About</a></li>
+                <li><a href="../notes.html">Production Notes</a></li>
+                <li><a href="../readcomic.html">Read Comic</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <main>
+        <section class="comic-page">
+            <h2></h2>
+        </section>
+
+        <div class="comic-page">
+            <button class="next-page-btn"><a href="1_PREV.html"><</a></button>
+            <img src="../pg/1_PAGE.png" alt="Page 1" class="page-image">
+            <button class="next-page-btn"><a href="1_NEXT
+        </div>
+
+
+    </main>
+
+
+
+</body>
+
+</html>
+    
+    '''
+
+    html_template = html_template.replace("PAGE", f'{page:03d}')
+    html_template = html_template.replace("PREV", f'{prev_page:03d}')
+
+    if(page !=num_pages):
+        html_template = html_template.replace("NEXT", f'{next_page:03d}.html">></a></button>')
+
+    else:
+        html_template = html_template.replace("NEXT",f'{page:03d}.html">.</a></button>')
+
+# Save the HTML to a file
+    with open(f'1_{page:03d}.html', 'w') as file:
+        file.write(html_template)
+
+print(f'{num_pages} HTML pages generated.')

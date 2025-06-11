c9b427085e27b28aecaef8c9b1a464b4d6f99b14
dsat4200 <kudosmydude@gmail.com>
2025-06-11 10:26:41 -0400

    much work is needed to make everything as smooth as possible

--- comics/3DTest/progress.gd
+++ comics/3DTest/progress.gd
@@ -4,11 +4,37 @@ extends AnimationPlayer
 @export var progress:float
 @export var start_anim: String
 @export var edit_mode:bool
+var selected = false;
 
 func _ready() -> void:
 	current_animation = start_anim
+	if Engine.is_editor_hint():
+		edit_mode = true
+	else:
+		edit_mode = false
+		set_path_preview(false)
 
+	
+	
+func set_path_preview(val:bool):
+	current_animation = start_anim
+	get_animation(current_animation).track_set_enabled(0,val)
+	
 func _process(delta: float) -> void:
-	if !edit_mode:
+	if !edit_mode and !selected:
 		progress = path_follow.progress_ratio*current_animation_length
 		seek(progress, true, true)
+
+
+func _on_point_data_clicked_point(index: Variant, progress: Variant) -> void:
+	pass # Replace with function body.
+	
+func select_point(p_ratio):
+	set_path_preview(false)
+	print("message recieved "+str(p_ratio))
+	selected = true
+	progress = p_ratio
+	
+func deselect_point():
+	set_path_preview(true)
+	selected = false
